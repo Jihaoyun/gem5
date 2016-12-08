2eb06966612a9b95bdec5699d6fd1436db996ccb
marioguerriero <marioguerriero33@gmail.com>
2016-12-08 12:27:59 +0100

    add parameters to gem5 executable

--- fault-injection-simulation.py
+++ fault-injection-simulation.py
@@ -3,6 +3,11 @@
 import argparse
 from subprocess import call
 
+import os, sys
+sys.path.append(os.getcwd() + "/configs/fault_injector")
+
+from FaultParser import *
+
 parser = argparse.ArgumentParser(description='Gem5')
 parser.add_argument('-b', '--benchmarks', type=str, dest='benchmarks',
                     nargs='+', help='Benchmark set for the simulation')
@@ -14,7 +19,17 @@ args = parser.parse_args()
 
 for benchmark in args.benchmarks:
     for inputFile in args.faultInput:
-        call(["./build/ALPHA/gem5.opt",
-            "configs/fault_injector/injector_system.py",
-            "-b", benchmark,
-            "-i", inputFile])
+        fp = FaultParser(inputFile)
+        while fp.hasNext():
+            fe = fp.next()
+            print "Running " + benchmark + " with fault:\n" + str(fe)
+            call(["./build/ALPHA/gem5.opt",
+                "configs/fault_injector/injector_system.py",
+                "-b", benchmark,
+                "-l", fe.label,
+                "-sb", fe.stuckBit,
+                "-f", fe.field,
+                "-e", fe.entry,
+                "-bp", fe.bitPosition,
+                "-tb", fe.tickBegin,
+                "-te", fe.tickEnd])
