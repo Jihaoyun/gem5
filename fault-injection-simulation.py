9abe317e5c8d9fe41ec90b31b5a69d42d2492c10
marioguerriero <marioguerriero33@gmail.com>
2016-12-30 14:03:13 +0100

    statistics can be plotted from command line, set some simulation script's parameters as mandatory

--- fault-injection-simulation.py
+++ fault-injection-simulation.py
@@ -10,62 +10,64 @@ from FaultParser import *
 
 parser = argparse.ArgumentParser(description='Gem5')
 parser.add_argument('-b', '--benchmarks', type=str, dest='benchmarks',
-                    nargs='+', help='Benchmark set for the simulation')
+                    required=True, nargs='+',
+                    help='Benchmark set for the simulation')
 
 parser.add_argument('-i', '--fault-input', type=str, dest='faultInput',
-                    nargs='+', help='Fault source files')
+                    required=True, nargs='+', help='Fault source files')
 
-parser.add_argument('-o', '--options', type=str, dest='options',
+parser.add_argument('-o', '--options', type=str, dest='options', required=True,
                     help='Options for the binary benchmark')
 
 args = parser.parse_args()
 
-# Run a simulation for each specified benchmark program
-for benchmark in args.benchmarks:
-    print "\n\nRunning " + benchmark + " GOLDEN\n"
+if __name__ == '__main__':
+    # Run a simulation for each specified benchmark program
+    for benchmark in args.benchmarks:
+        print "\n\nRunning " + benchmark + " GOLDEN\n"
 
-    if args.options != None:
-        benchmark = " ".join([benchmark, args.options])
+        if args.options != None:
+            benchmark = " ".join([benchmark, args.options])
 
-    # Create a folder to store stats releated to the current benchmark
-    statFolder = benchmark.split("/")[-1]
-    if not os.path.exists("m5out/" + statFolder):
-        os.makedirs("m5out/" + statFolder)
+        # Create a folder to store stats releated to the current benchmark
+        statFolder = benchmark.split("/")[-1]
+        if not os.path.exists("m5out/" + statFolder):
+            os.makedirs("m5out/" + statFolder)
 
-    # Run Golden simulation
-    cmd = ["./build/ALPHA/gem5.opt",
-        "--stats-file", statFolder + "/" +
-        "GOLDEN.txt",
-        "configs/fault_injector/injector_system.py",
-        "-b", benchmark ]
+        # Run Golden simulation
+        cmd = ["./build/ALPHA/gem5.opt",
+            "--stats-file", statFolder + "/" +
+            "GOLDEN.txt",
+            "configs/fault_injector/injector_system.py",
+            "-b", benchmark ]
 
-    call(cmd)
+        call(cmd)
 
-    # Read all fault input files
-    if args.faultInput == None:
-        break
+        # Read all fault input files
+        if args.faultInput == None:
+            break
 
-    for inputFile in args.faultInput:
-        fp = FaultParser(inputFile)
-        while fp.hasNext():
-            # Load the next fault entry
-            fe = fp.next()
+        for inputFile in args.faultInput:
+            fp = FaultParser(inputFile)
+            while fp.hasNext():
+                # Load the next fault entry
+                fe = fp.next()
 
-            print "\n\nRunning " + benchmark + " with fault:\n" + str(fe)
+                print "\n\nRunning " + benchmark + " with fault:\n" + str(fe)
 
-            # Run faulted simulation
-            cmd = ["./build/ALPHA/gem5.opt",
-                "--stats-file", statFolder + "/" +
-                fe.label + ".txt",
-                "configs/fault_injector/injector_system.py",
-                "-fe",
-                "-b", benchmark,
-                "-l", fe.label,
-                "-sb", fe.stuckBit,
-                "-f", fe.field,
-                "-e", fe.entry,
-                "-bp", fe.bitPosition,
-                "-tb", fe.tickBegin,
-                "-te", fe.tickEnd]
+                # Run faulted simulation
+                cmd = ["./build/ALPHA/gem5.opt",
+                    "--stats-file", statFolder + "/" +
+                    fe.label + ".txt",
+                    "configs/fault_injector/injector_system.py",
+                    "-fe",
+                    "-b", benchmark,
+                    "-l", fe.label,
+                    "-sb", fe.stuckBit,
+                    "-f", fe.field,
+                    "-e", fe.entry,
+                    "-bp", fe.bitPosition,
+                    "-tb", fe.tickBegin,
+                    "-te", fe.tickEnd]
 
-            call(cmd)
+                call(cmd)
--- stats.py
+++ stats.py
@@ -13,9 +13,21 @@ from os.path import isdir
 
 import argparse
 
+# GOLDEN instance reference name
+GOLDEN = 'GOLDEN.txt'
+
+# Command line arguments
 parser = argparse.ArgumentParser(description='Gem5 Stats')
-parser.add_argument('-d', '--dir', type=str, dest='directory',
+parser.add_argument('-d', '--dir', type=str, dest='directory', required=True,
                     help='The root directory of the stats files to be parsed')
+
+parser.add_argument('-g', '--graphical', dest='graphicalStats',
+                    action='store_true',
+                    help='It is true if we want to display graphical stats')
+parser.set_defaults(graphicalStats=False)
+
+parser.add_argument('-s', '--stats', type=str, dest='stats', required=True,
+                    nargs='+', help='The statistics we want to display')
 
 args = parser.parse_args()
 
@@ -25,19 +37,51 @@ args = parser.parse_args()
 statInstances = {}
 
 # Print some simple statistics
-def simplestat(stats, props):
+def printstat(stats, props):
     for p in props:
         print p, "values"
         for inst in stats:
-            print inst, stats[inst][p]
+            print inst, stats[inst].get(p, 'not found')
+
+# Display a barchart displaying the given statistics
+def showgraph(stats, props):
+    # Iterate over all stats
+    for p in props:
+        # Store labels for the horizontal axis
+        labels = []
+        # Store values fro the vertical axis
+        values = []
+        for inst in stats:
+            labels.append(inst)
+            values.append(stats[inst].get(p, 0))
+
+        # Put GOLDEN value at first position
+        idx = labels.index(GOLDEN)
+        labels[idx], labels[0] = labels[0], labels[idx]
+        values[idx], values[0] = values[0], values[idx]
+
+        # Plot labels and values
+        fig, ax = plt.subplots()
+        rects = ax.bar(np.arange(len(labels)), tuple(values),
+            width=0.3, color='r')
+        ax.set_ylabel(p)
+        ax.set_title('Variation of ' + p)
+        ax.set_xticklabels(tuple(labels))
+        ax.set_xticks(np.arange(len(labels)) + 0.3 / 2)
+        autolabel(rects, ax)
+        plt.show()
+
+# Attach a text label above each bar displaying its height
+def autolabel(rects, ax):
+    for rect in rects:
+        height = rect.get_height()
+        ax.text(rect.get_x() + rect.get_width()/2., 1.05*height,
+                '%d' % int(height),
+                ha='center', va='bottom')
 
 # List all files in the directory
 if __name__ == "__main__":
-    # Exit if wrong usage is detected
-    if args.directory == None:
-        print "Usage: ./stats.py -d STAT_ROOT_DIR"
-        sys.exit(1)
-
+    # Load statistics
     # Star by listing all the files in the directory
     for f in listdir(args.directory):
         # If the file is a directory skip it
@@ -68,3 +112,15 @@ if __name__ == "__main__":
 
             # Store the stat object
             statInstances[f] = stat
+
+    # If we are in command line mode just display the requested statistics
+    if not args.graphicalStats:
+        printstat(statInstances, args.stats)
+    # Else show some graphics
+    else:
+        # Import the requested libraries
+        import numpy as np
+        import matplotlib.pyplot as plt
+
+        # Plot stats
+        showgraph(statInstances, args.stats)
